ddbf3196ea7de822fbcd6ae0a23f52180325fbd0
MauroMCarvalho <53716103+MauroMCarvalho@users.noreply.github.com>
2023-02-20 12:32:04 -0300

    código ajustado

--- src/utils/tokens.js
+++
@@ -1,9 +0,0 @@
-
-
-const tokenGeocode = 'pk.eyJ1IjoibWF1cm9tY2JyIiwiYSI6ImNsZWJndDBhcDAzM28zeHF0czA5cndkNzMifQ.Et9QtvjfZgilNQqKoOuGMA'
-const tokenForecast = '3aa0afb074db7b7ea76d4200d6f7aa1f'
-
-module.exports = {
-    tokenGeocode,
-    tokenForecast
-}
